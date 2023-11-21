c15169a3ee29c56580af50cc556d5828fae5484a
andersonjeccel <116097999+andersonjeccel@users.noreply.github.com>
2023-11-21 11:14:06 -0300

    Update admin-interface.php

--- WordPress/plugins/odyssey-restrict/admin-interface.php
+++ WordPress/plugins/odyssey-restrict/admin-interface.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h2>Odyssey Restrict Settings</h2>
+    <h2>Odyssey Restrict</h2>
     <form method="post" action="options.php">
         <?php
             settings_fields('odyssey_restrict_options');
@@ -7,31 +7,39 @@
         ?>
         <table class="form-table">
             <tr valign="top">
-                <th scope="row">Allow Homepage</th>
+                <th scope="row">Tornar a página inicial pública</th>
                 <td><input type="checkbox" name="odyssey_restrict_allow_home" value="yes" <?php checked('yes', get_option('odyssey_restrict_allow_home', 'no')); ?> /></td>
             </tr>
             <tr valign="top">
-                <th scope="row">Allowed Pages</th>
+                <th scope="row">Páginas públicas</th>
                 <td>
                     <?php
                         $allowed_pages = get_option('odyssey_restrict_allowed_pages', []);
                         $pages = get_pages();
-                        $posts = get_posts();
 
                         foreach ($pages as $page) {
                             echo '<input type="checkbox" name="odyssey_restrict_allowed_pages[]" value="' . $page->ID . '" ' . (in_array($page->ID, $allowed_pages) ? 'checked' : '') . ' />' . $page->post_title . '<br>';
                         }
+                    ?>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Posts públicos</th>
+                <td>
+                    <?php
+                        $allowed_posts = get_option('odyssey_restrict_allowed_posts', []);
+                        $posts = get_posts();
 
                         foreach ($posts as $post) {
-                            echo '<input type="checkbox" name="odyssey_restrict_allowed_pages[]" value="' . $post->ID . '" ' . (in_array($post->ID, $allowed_pages) ? 'checked' : '') . ' />' . $post->post_title . '<br>';
+                            echo '<input type="checkbox" name="odyssey_restrict_allowed_posts[]" value="' . $post->ID . '" ' . (in_array($post->ID, $allowed_posts) ? 'checked' : '') . ' />' . $post->post_title . '<br>';
                         }
                     ?>
                 </td>
             </tr>
         </table>
-        <input type="submit" class="button-primary" value="Save Changes" />
+        <input type="submit" class="button-primary" value="Salvar alterações" />
     </form>
     <form method="post">
-        <input type="submit" name="reset_odyssey_restrict_settings" class="button" value="Corrigir Configurações" />
+        <input type="submit" name="reset_odyssey_restrict_settings" class="button" value="Corrigir configurações" />
     </form>
 </div>
