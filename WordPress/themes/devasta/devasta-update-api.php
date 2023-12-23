bec0494b2554b22aeb3c73b5c76add3cf840b81b
andersonjeccel <116097999+andersonjeccel@users.noreply.github.com>
2023-12-23 11:46:03 -0300

    Separação dos arquivos para outros repositórios

--- WordPress/themes/devasta/devasta-update-api.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = array(
-    'version' => '1.2.15',
-    'download_url' => 'https://updates.idmotion.com.br/devasta/devasta-1.2.15.zip'
-);
-header('Content-Type: application/json');
-echo json_encode($data);
-?>
